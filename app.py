983ba486a4f89b5391c3fdb4a42c837a377a2d59
atharvaujoshicric-dev <atharvaujoshicric@gmail.com>
2026-02-03 12:06:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if uploaded_file:
         if uploaded_file.name.endswith('.csv'):
             df = pd.read_csv(uploaded_file)
         else:
-            df = pd.read_excel(uploaded_file, sheet_name='summary')
+            df = pd.read_excel(uploaded_file, sheet_name='Summary')
 
         st.success("File uploaded successfully! Processing data...")
 
