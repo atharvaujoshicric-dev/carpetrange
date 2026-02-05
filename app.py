73618ea13fdccede0e1682f954f7baf12b10df93
atharvaujoshicric-dev <atharvaujoshicric@gmail.com>
2026-02-05 18:42:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ st.set_page_config(page_title="Property Report Tool", layout="wide")
 st.title("🏙️Spydarr's Summary to Report")
 
 st.markdown("""
-<div style='background-color: #f0f2f6; padding: 10px; border-radius: 5px; border-left: 5px solid #ff4b4b; margin-bottom: 20px;'>
+
     <strong>Need the base summary first?</strong>
     st.markdown("<div style='margin-top: -15px; margin-bottom: 10px;'><span style='background-color: #FFFF00; padding: 2px 8px; border-radius: 4px; border: 1px solid #E6E600; font-size: 0.9em; color: black;'><u><strong>NOTE :-</strong> Before Uploading the Summary Cross-Check it.</u></span></div>", unsafe_allow_html=True)
     <a href="https://spydarr.streamlit.app/" target="_blank">To get the summary click here: Spydarr Dashboard</a>
